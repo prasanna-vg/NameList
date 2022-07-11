47f9d8eb33c70f94fd5b8ab092b79f1bc7d810c0
Prasanna <prasannavg000@gmail.com>
2022-07-11 16:12:27 +0530

    Added env

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var path = require('path');
 var mongoose = require('mongoose');
+require('dotenv').config();
 
 var app = express();
-const port = 3000;
+const port = process.env.port || 3000;
+
+const dbUrl = process.env.MONGO_URL;
+const ConnectionParams = {
+    useNewUrlParser: true, useUnifiedTopology: true
+}
 //Connect to mongoDB
-mongoose.connect('mongodb://localhost:27017/namelist');
-//On Connection
-mongoose.connection.on('connected',()=>{
-    console.log("Connected to DB at 27017");
+mongoose.connect(dbUrl,ConnectionParams).then(()=>{
+    console.log("connected to Online DB");
+}).catch((err)=>{
+    console.log(err);
 });
+//Connect to mongoDB
+//mongoose.connect('mongodb://localhost:27017/namelist');
+
+//On Connection
+// mongoose.connection.on('connected',()=>{
+//     console.log("Connected to DB at 27017");
+// });
 //On Error
 mongoose.connection.on('error',(err)=>{
     if(err){
