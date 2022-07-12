1e76d025196699f007e5a08f6b6ad600dc98298d
prasanna-vg <56516795+prasanna-vg@users.noreply.github.com>
2022-07-12 15:15:56 +0530

    port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const dbUrl = process.env.MONGO_URL;
 const ConnectionParams = {
